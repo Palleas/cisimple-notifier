c084bcbf2de6210483d0a27a126ace69b6da5e74
Romain Pouclet <palleas@gmail.com>
2013-02-12 12:04:42 -0500

    Handling build phases

--- CISimple-notifier/CISBuild.h
+++ CISimple-notifier/CISBuild.h
@@ -8,10 +8,17 @@
 
 #import <Foundation/Foundation.h>
 
+NS_ENUM(int, CISBuildPhase) {
+    CISBuildPhaseStarted = 0,
+    CISBuildPhaseCompleted = 1,
+    CISBuildPhaseFinished = 2
+};
+
 @interface CISBuild : NSObject
 
 @property (nonatomic, assign) NSNumber *buildNumber;
 @property (nonatomic, strong) NSString *projectName;
+@property (nonatomic, assign) enum CISBuildPhase phase;
 @property (nonatomic, assign) BOOL success;
 
 + (CISBuild *)buildWithDictionnary:(NSDictionary *)dict;
